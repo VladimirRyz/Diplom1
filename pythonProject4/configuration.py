c2c50de0c68676109d7b085331df385f4fcb5fe6
VladimirRyz <142989381+VladimirRyz@users.noreply.github.com>
2024-05-13 10:12:57 +0300

    Add commit 2

--- pythonProject4/configuration.py
+++ pythonProject4/configuration.py
@@ -1,5 +1,5 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = 'https://c5df6328-dfc5-4f1e-807c-0c57695405fe.serverhub.praktikum-services.ru'
+URL_SERVICE = 'https://e121a682-92f8-47e8-8b5d-4c3e7fdea783.serverhub.praktikum-services.ru'
 # CREATE_NEW_ORDERS хранит путь к API-методу для создания нового заказа.
 CREATE_NEW_ORDERS = '/api/v1/orders/'
 # READ_ORDERS_TRACK хранит путь к API-методу для получения заказа по трекеру
