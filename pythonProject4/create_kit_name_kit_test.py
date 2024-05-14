0ea3eb937a7bfd0b6fbcfa232402c69f311914b3
VladimirRyz <142989381+VladimirRyz@users.noreply.github.com>
2024-05-14 09:32:53 +0300

    close perem

--- pythonProject4/create_kit_name_kit_test.py
+++ pythonProject4/create_kit_name_kit_test.py
@@ -13,18 +13,10 @@ def test_new_orders():
     #заменяю значение номера заказа в параметрах для запроса заказа по номеру
     data.orders_track['t'] = track
     #Проверяю, что код ответа равен 201
-    if new_track.status_code == 201:
-        print("Тест пройден")
-    else:
-        print("Тест провален")
     assert new_track.status_code == 201
 
 
 def test_orders_track():
     sender_stand_request.get_orders_track()
     order_response = sender_stand_request.get_orders_track()
-    if order_response.status_code == 200:
-        print("Тест пройден")
-    else:
-        print("Тест провален")
     assert order_response.status_code == 200
--- pythonProject4/sender_stand_request.py
+++ pythonProject4/sender_stand_request.py
@@ -6,12 +6,10 @@ import data
 def post_new_orders():
     url = configuration.URL_SERVICE + configuration.CREATE_NEW_ORDERS
     body = data.new_orders
-    headers = data.headers
-    return requests.post(url, json=body, headers=headers)
+    return requests.post(url, json=body, headers=data.headers)
 
 
 def get_orders_track():
     url = configuration.URL_SERVICE + configuration.READ_ORDERS_TRACK
     new_body = data.orders_track.copy()
-    headers = data.headers.copy()
-    return requests.get(url, params=new_body, headers=headers)
+    return requests.get(url, params=new_body, headers=data.headers)
